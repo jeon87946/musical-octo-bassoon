c4df92a8f0792df86e3f606ea8a3426a10d8a976
DESKTOP-O3LMC10\User <jb_0162@naver.com>
2024-03-05 11:54:35 +0900

    0305

---
+++ java1/src/kr/ac/kopo/day01/HelloMain.java
@@ -0,0 +1,17 @@
+package kr.ac.kopo.day01;
+
+public class HelloMain {
+
+  public static void main(String[] args) {
+	System.out.println(false);
+	
+	
+	System.out.print(3);
+	System.out.print(' ');
+	System.out.print('+');
+	System.out.print(' ');
+	System.out.print(4);
+	System.out.println();
+	System.out.println(3 + ' ' + ' ' + 4);
+	}
+}
